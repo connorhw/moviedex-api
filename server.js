b2a8cf72356ac57103f5f452a534e2aba0d43df9
connorhw <connorwong93@gmail.com>
2020-04-11 15:34:54 -0700

    first submission

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+//const helmet = require('helmet')
 const MOVIES = require('./moviedex.json')
 
+console.log(process.env.API_TOKEN)
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+//app.use(helmet())
 
 
-const validTypes = []
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
 
+
+/*
 function handleGetMovies(req, res) {
     let response = MOVIES;
     res
       .send(response)
 }
-app.get('/movie', handleGetMovies)
+*/
+app.get('/movie', function handleGetMovies(req, res) {
+  let response = MOVIES;
+  if (req.query.genre) {
+    response = response.filter(movie =>
+        movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+      )
+  }
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+  
+  
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
+  
+  res.json(response)
+})
 
 //app.use(morgan('common')); // let's see what 'common' format looks like
 //const movies = require('./moviestore.js')
@@ -36,7 +73,7 @@ app.use((req, res) => {
 })
 */
 
-const PORT = 8000
+const PORT = 9000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
