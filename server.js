ab20cee2b30d56904d0098adcf97e7ab764ee3f9
connorhw <connorwong93@gmail.com>
2020-04-16 01:53:19 -0700

    last commit before first deploy attempt.

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const MOVIES = require('./moviedex.json')
 console.log(process.env.API_TOKEN)
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 //app.use(helmet())
 
@@ -72,9 +73,19 @@ app.use((req, res) => {
   res.send('server js is working!')
 })
 */
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
-const PORT = 9000
+
+const PORT = process.env.PORT || 9000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  //console.log(`Server listening at http://localhost:${PORT}`)
 })
